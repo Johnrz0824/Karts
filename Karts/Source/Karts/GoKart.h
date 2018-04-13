a522d06ad0d48a9bb105a6ebe6aa4cfa6797ec4c
Hector Hung <johnrz0824@gmail.com>
2018-04-13 17:30:01 +0800

    Move

---
+++ Karts/Source/Karts/GoKart.h
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "GoKart.generated.h"
+
+UCLASS()
+class KARTS_API AGoKart : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	AGoKart();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	float Throttle;
+	FVector Velocity;
+	void MoveForawrd(float Value);
+	UPROPERTY(EditAnywhere)
+		float Mass = 1000;
+
+	UPROPERTY(EditAnywhere)
+		float MaxDrivingForce = 10000;
+	void UpdateLocationFromVelocity(float DeltaTime);
+};
